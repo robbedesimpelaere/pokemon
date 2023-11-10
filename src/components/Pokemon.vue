7cb442c62f9c164b1cac067c3ef50470f38030ee
robbedesimpelaere <robbe.desimpelaere@student.howest.be>
2023-11-10 16:19:16 +0100

    feat: img_ready

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,6 +4,7 @@
   <p>
     {{ pokemonData.name }}
   </p>
+  <img :src="getPokemonImageUrl()" :alt="`foto ${pokemonData.name}`" />
 </template>
 
 <script setup>
@@ -13,4 +14,16 @@ const props = defineProps({
     required: true,
   },
 });
+
+const getPokemonId = function () {
+  const pokemonId = props.pokemonData.url.split('/')[props.pokemonData.url.split('/').length - 2];
+  console.log(props.pokemonData.url, pokemonId);
+  return pokemonId;
+};
+
+const getPokemonImageUrl = function () {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${getPokemonId()}.gif`;
+};
+
+getPokemonId();
 </script>
