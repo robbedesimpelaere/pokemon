216d656c31ddfb49b6eca81f2d7c22c88315cbc2
robbedesimpelaere <robbe.desimpelaere@student.howest.be>
2023-11-10 15:54:06 +0100

    feat: pokemon_component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
+  <logo />
+
   <h1>Pokemon!</h1>
+
+  <div>
+    <pokemon v-for="pokemon in pokemons"
+     :pokemonData="pokemon" 
+     :key="pokemon.name"
+     > 
+    </pokemon>
+  </div>
 </template>
 
 <script setup>
-const getPokemons = async function (limit = 100000) {
+import { ref } from 'vue';
+import logo from './components/Logo.vue';
+import pokemon from './components/Pokemon.vue';
+
+const pokemons = ref([]);
+
+const getPokemons = async function (limit = 12) {
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`).then((r) => r.json());
+  console.log(data);
+  pokemons.value = data.results;
 };
+
+getPokemons();
 </script>
 
 <style lang="scss">
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <p>
+    {{ pokemonData.name }}
+  </p>
+</template>
+
+<script setup>
+const props = defineProps({
+  pokemonData: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
