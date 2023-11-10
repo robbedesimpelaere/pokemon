2737e066c089f9ec524a308b7c043e6f53373af5
robbedesimpelaere <robbe.desimpelaere@student.howest.be>
2023-11-10 15:14:21 +0100

    feat: ready

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Pokemon!</h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+const getPokemons = async function (limit = 100000) {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`).then((r) => r.json());
+};
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style lang="scss">
+html {
+  color: #fefefe;
+  background: #303030;
+  font-family: 'Work Sans', sans-serif;
 }
 </style>
